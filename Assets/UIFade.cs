f0b82fd9f8a908079c584545febddb55d021c398
Hitori <codename.virtualentity14321@gmail.com>
2023-06-08 13:48:27 +0700

    [any]

---
+++ Assets/UIFade.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFade : UIAnimate
+{
+    protected override void PreActivate()
+    {
+        base.PreActivate();
+    }
+    protected override void PostActivate()
+    {
+        base.PostActivate();
+    }
+    protected override void Activate(float duration)
+    {
+        base.Activate(duration);
+    }
+    protected override void PreDeactivate()
+    {
+        base.PreDeactivate();
+    }
+
+    protected override void PostDeactivate()
+    {
+        base.PostDeactivate();
+    }
+    protected override void Deactivate(float duration)
+    {
+        base.Deactivate(duration);
+    }
+}
